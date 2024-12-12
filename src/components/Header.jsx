8eaae8b2d717987cb35588a57d3cd64627c667ff
Sreejith.R <shrsree341o@gmail.com>
2024-12-12 23:11:51 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
           </div>
         ) : (
           <button
-            className="bg-yellow py-1 px-5 text-white  font-semibold text-xs cursor-pointer rounded border-2 border-transparent hover:border-yellow hover:bg-transparent hover:text-yellow duration-200"
+            className="bg-slate-900 py-1 px-5 text-white  font-semibold text-xs cursor-pointer rounded border-2 border-transparent hover:border-yellow hover:bg-transparent hover:text-yellow duration-200"
             onClick={login}
           >
             Login
